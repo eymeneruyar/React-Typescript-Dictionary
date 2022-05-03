9b6e1a12712e85301a16061ec0aebade9a10913e
Eymen ERUYAR <88077955+eymeneruyar@users.noreply.github.com>
2022-05-04 00:39:47 +0300

    Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 import Header from '../inc/Header'
 import { MdTranslate, MdSearch } from 'react-icons/md';
-
+//TODO: Searc bar tekrar eden bir yapı olduğu için component halinde kullanılabilir.
 
 export default function Home() {
     return (
@@ -37,6 +37,20 @@ export default function Home() {
                                 <span className='button_icon'><MdSearch size={35}/></span>
                             </Button>
                         </Form>
+                    </div>
+                    <div className='subpage_search_result active'>
+                        <ul className='subpage_search_result_list'>
+                            <li><a>Örnek1</a></li>
+                            <li><a>Örnek2</a></li>
+                            <li><a>Örnek3</a></li>
+                        </ul>
+                    </div>
+                    <div className='dictionary_header'>
+                        <div className='dictionary_header_title'>
+                            <span style={{color:"#4545a2"}}>Günün </span>
+                            <span style={{color:"#21325a"}}>Kelimesi</span>
+                        </div>
+                        <span className='dictionary_header_date' style={{color:"#4545a2"}}>4 Mayıs 2022</span>
                     </div>
                 </div>
                 
